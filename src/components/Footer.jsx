be06122c4fcd9c294095ec713cffa80a0d08547c
marta-santos <martasilvaa325@gmail.com>
2022-12-07 18:44:36 -0300

    mudaças no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ export function Footer() {
         <footer className={styles.footer}>
             <div className={styles.div}>
                 <a href="https://www.linkedin.com/in/marta-santos-4a8309194/" target="_blank" >
-                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/4701/4701533.png" />
+                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/4701/4701533.png" alt="icon de do linkedin com in" />
                 </a>
                 <a href="mailto:martasilvaa325@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/4926/4926635.png" alt="" />
+                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/4926/4926635.png" alt="icon de formato de d M" />
                 </a>
                 <a href="https://www.gov.br/saude/pt-br/composicao/saes/sangue/lista-de-hemocentros-no-brasil" target="_blank" rel="noopener noreferrer">
-                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/1876/1876556.png" alt="" />
+                    <img className={styles.icon} src="https://cdn-icons-png.flaticon.com/128/1876/1876556.png" alt="icon de contatos" />
                 </a>
             </div>
         </footer>
